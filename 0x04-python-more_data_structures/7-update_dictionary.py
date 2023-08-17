753a4f4a2e21e9228b4d7cc9dd397dd86604889d
Khozacharles832 <khozacharles832@gmail.com>
2023-08-17 10:04:59 -0700

    fix control statement

--- 0x04-python-more_data_structures/7-update_dictionary.py
+++ 0x04-python-more_data_structures/7-update_dictionary.py
@@ -1,4 +1,9 @@
 #!/usr/bin/python3
 def update_dictionary(a_dictionary, key, value):
-    a_dictionary[key] = value
+    if key not in a_dictionary:
+        a_dictionary[key] = value
+    else:
+        for up in a_dictionary:
+            if up == key:
+                a_dictionary[up] = value
     return (a_dictionary)

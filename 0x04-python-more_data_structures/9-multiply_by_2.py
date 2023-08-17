dd013803fb65741b44a7605e5c917fc920eef785
Khozacharles832 <khozacharles832@gmail.com>
2023-08-17 10:22:15 -0700

    add copy function

--- 0x04-python-more_data_structures/9-multiply_by_2.py
+++ 0x04-python-more_data_structures/9-multiply_by_2.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 def multiply_by_2(a_dictionary):
-    _new = a_dictionary.copy()
-    for key, value in _new.items():
-        _new[key] = value * 2
-    return (_new)
+    res = a_dictionary.copy()
+    _list = list(res.keys())
+    for i in _list:
+        res[i] *= 2
+    return (res)

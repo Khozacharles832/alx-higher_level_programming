99e74ddc54e92c95a62003da7d95eae099d9bdf5
Khozacharles832 <khozacharles832@gmail.com>
2023-08-17 09:13:40 -0700

    fix bug

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def search_replace(my_list, search, replace):
     _list = []
-    for i in _list:
+    for i in my_list:
         if i != search:
             _list.append(i)
         else:

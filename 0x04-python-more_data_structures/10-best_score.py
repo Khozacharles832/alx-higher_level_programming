1e6f39f0711feb92212e349187e33bd4977840cf
Khozacharles832 <khozacharles832@gmail.com>
2023-08-17 10:45:28 -0700

    add items function

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,4 +1,10 @@
 def best_score(a_dictionary):
     if not a_dictionary:
         return (None)
-    return (max(a_dictionary, key=a_dictionary.get))
+    _large = None
+    _max = float('-inf')
+    for key, value in a_dictionary.items():
+        if value > _max:
+            _large = key
+            _max = value
+    return (_large)

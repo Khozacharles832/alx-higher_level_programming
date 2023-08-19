4557f6c1d02245e5334f41fd45c28e76ea0933f3
Khozacharles832 <khozacharles832@gmail.com>
2023-08-18 18:45:50 -0700

    fix bug

--- 0x04-python-more_data_structures/5-number_keys.py
+++ 0x04-python-more_data_structures/5-number_keys.py
@@ -1,7 +1,3 @@
 #!/usr/bin/pyhon3
 def number_keys(a_dictionary):
-    res = 0
-    _keys = list(a_dictionary.keys())
-    for i in _keys:
-        res += 1
-    return (res)
+    return len(a_dictionary.keys())

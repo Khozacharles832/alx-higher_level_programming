bbf97e3aca52edcaa5d0c6fdae631e6a57b528c0
Khozacharles832 <khozacharles832@gmail.com>
2023-08-30 05:12:39 -0700

    add comments

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -1,19 +1,44 @@
 #!/usr/bin/python3
+""" Defines a class square """
+
 
 class Square:
+    """ class that defines the properties of a square by: (5-square.py)
+        
+        Attributes:
+            size: size of the square (1 side).
+    """
     def __init__(self, size=0, position=(0, 0)):
         self.size = size
         self.position = position
 
     def area(self):
+        """ calculates the area of the square
+
+            Returns:
+                the area of the square
+        """
         return (self.__size ** 2)
 
     @property
     def size(self):
+        """
+            Returns the size of the square
+        """
         return (self.__size)
 
     @size.setter
     def size(self, data):
+        """
+            property setter for size
+
+            Args:
+                value (int): size of a square (1 side).
+
+            Raises:
+                TypeError: size must be an integer
+                ValueError: size must be >= 0
+        """
         if not isinstance(data, int):
             raise TypeError("size must be an integer")
         if data < 0:
@@ -22,10 +47,20 @@ class Square:
 
     @property
     def position(self):
+        """ Returns the position of the square
+        """
         return (self.__position)
 
     @position.setter
     def position(self, data):
+        """ property setter for position
+
+        Args:
+            value (tuple): position of the square
+
+        Raises:
+            TypeError: position must be a tuple of 2 positive integers
+        """
         if not isinstance(data, tuple):
             raise TypeError("position must be a tuple of 2 positive integers")
         if len(data) != 2:
@@ -37,6 +72,9 @@ class Square:
         self.__position = data
 
     def my_print(self):
+        """
+            prints the # char to stdout
+        """
         if self.__size == 0:
             print()
         else:

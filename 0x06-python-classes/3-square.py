6c0bec04a0c0b1f175aa6e9d2d5cb8ac93f2151f
Khozacharles832 <khozacharles832@gmail.com>
2023-08-30 03:56:47 -0700

    fix documentation

--- 0x06-python-classes/3-square.py
+++ 0x06-python-classes/3-square.py
@@ -1,7 +1,21 @@
 #!/usr/bin/python3
+"""" Defines a class square"""
+
 
 class Square:
+    """
+    class that defines properties of a square by: (based on 2-square.py)
+
+    Attributes:
+        size: size of square ( side).
+    """
     def __init__(self, size=0):
+        """
+        create a new instance of square
+
+        Args:
+            size: the size of square (1 side).
+        """
         if not isinstance(size, int):
             raise TypeError("size must be an integer")
         if size < 0:
@@ -9,4 +23,8 @@ class Square:
         self.__size = size
 
     def area(self):
+        """ calculates the length of the square
+
+        Returns: the current square area
+        """
         return (self.__size ** 2)

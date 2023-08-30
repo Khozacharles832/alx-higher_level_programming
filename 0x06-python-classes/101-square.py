c981944267b10ab5db4ff0431402fd8c5eb15216
Khozacharles832 <khozacharles832@gmail.com>
2023-08-30 06:42:26 -0700

    fix documentation

--- 0x06-python-classes/101-square.py
+++ 0x06-python-classes/101-square.py
@@ -1,19 +1,46 @@
 #!/usr/bin/python3
+""" Defines a class square """
 
 class Square:
+    """
+    class that defines properties of by(based on 3-square.py).
+
+    Attributes:
+        size: the size of the square
+    """
     def __init__(self, size=0, position=(0, 0)):
+        """ creates new instance of square
+
+        Attributes:
+            size: the size of the square (1 side).
+        """
         self.size = size
         self.position = position
 
     def area(self):
+        """calculates the area of the square
+
+        Returns: the area of the square
+        """
         return (self.__size ** 2)
 
     @property
     def size(self):
+        """Returns the size of the square
+        """
         return (self.__size)
 
     @size.setter
     def size(self, data):
+        """property setter for size
+
+        Args:
+            value (int): the single size of the square
+
+        Raises:
+            TypeError: size must be an integer
+            ValueError: size must be >= 0
+        """
         if not isinstance(data, int):
             raise TypeError("size must be an integer")
         if data < 0:
@@ -22,10 +49,22 @@ class Square:
 
     @property
     def position(self):
+        """ sets the position
+
+        Returns: the position
+        """
         return (self.__position)
 
     @position.setter
     def position(self, data):
+        """ the postition setter
+
+        Args:
+            data:the dat
+
+        Raises:
+            TypeError: position must be a tuple of 2 positive integers
+        """
         if not isinstance(data, tuple):
             raise TypeError("position must be a tuple of 2 positive integers")
         if len(data) != 2:
@@ -37,6 +76,11 @@ class Square:
         self.__position = data
 
     def my_print(self):
+        """prints elements of a linked list
+
+        Args:
+            size: the size to be printed
+        """
         if self.__size == 0:
             print()
         else:
@@ -48,6 +92,11 @@ class Square:
                 print("#" * self.__size)
 
     def __str__(self):
+        """sets the position
+
+        Args:
+            size: the size
+        """
         if self.__size == 0:
             return ('')
         positions = '\n' * self.position[1]

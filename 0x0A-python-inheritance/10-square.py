a786e2703bcad9baffa62b9edbe006e9f5d87f4c
Khoza Charles <khozacharles832@gmail.com>
2023-12-30 18:23:31 -0800

    fix bug

--- 0x0A-python-inheritance/10-square.py
+++ 0x0A-python-inheritance/10-square.py
@@ -1,17 +1,23 @@
 #!/usr/bin/python3
-""" The Rectangles child class """
-
-
 Rectangle = __import__('9-rectangle').Rectangle
+"""
+===================================
+module with class BaseGeometry
+===================================
+"""
 
 
 class Square(Rectangle):
-    """ The square class """
+    """Square class that inherits from Rectangle that inherits BaseGeometry"""
+
     def __init__(self, size):
-        self.__size = size
-        BaseGeometry.integer_validator(self, "size", size)
+        """Method for initialized the attrubutes"""
+
         super().__init__(size, size)
+        self.integer_validator("size", size)
+        self.__size = size
 
     def area(self):
-        """ the size method """
+        """rectangle area"""
+
         return self.__size ** 2
